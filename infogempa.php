bb61d46fea17818c5aa7ca1ec0ef31a5741dafc3
Dimas Agung Prabowo <96804772+dimasagp1@users.noreply.github.com>
2024-03-08 15:43:34 +0700

    Update infogempa.php

--- infogempa.php
+++ infogempa.php
@@ -1,38 +1,37 @@
-
 <?php
 $url="https://data.bmkg.go.id/DataMKG/TEWS/gempadirasakan.xml";
-$gempa= new SimpleXMLElement ($url,null,true);
-//echo "<pre>";print_r($gempa);
+$gempa = new SimpleXMLElement(file_get_contents($url));
 
-echo "<h2>INFO GEMPA</h2>";
-$tmp="<table border='1'>
-        <tr><td> TANGGAL </td>
-        <td> JAM </td>
-        <td> TANGGAL WAKTU </td>
-        <td> KOORDINAT </td>
-        <td> LINTANG </td>
-        <td> BUJUR </td>
-        <td> Magnitude </td>
-        <td> KEDALAMAN </td>
-        <td> WILAYAH </td>
-        <td> DIRASAKAN </td>
-        <tr>";
 
-    for($i=0;$i<count($gempa->gempa);$i++) {
-        $tmp.="
-        <tr><td>{$gempa->gempa[$i]->Tanggal}</td>
-        <td><b>{$gempa->gempa[$i]->Jam}</b></td>
-        <td><b>{$gempa->gempa[$i]->DateTime}</b></td>
-        <td><b>{$gempa->gempa[$i]->point->coordinates}</b></td>
-        <td><b>{$gempa->gempa[$i]->Lintang}</b></td>
-        <td><b>{$gempa->gempa[$i]->Bujur}</b></td>
-        <td><b>{$gempa->gempa[$i]->Magnitude}</b></td>
-        <td><b>{$gempa->gempa[$i]->Kedalaman}</b></td>
-        <td><b>{$gempa->gempa[$i]->Wilayah}</b></td>
-        <td><b>{$gempa->gempa[$i]->Dirasakan}</b></td>
-        
-        </tr>";
-    }
-    $tmp.="</table>"; 
-    echo $tmp;
+echo "<b><tr>  NAMA              : DIMAS AGUNG PRABOWO </tr></b><br/>";
+echo "<b><tr>  NIM               : 12211834 </tr></b><br/>";
+echo "<b><tr>  PRODI             : INFORMATIKA</tr></b><br/>";
+echo "<b><tr>  PERGURUAN TINGGI  : STMIK ELRAHMA YK</tr></b><br/>";
+
+
+echo "<h2>INFORMASI GEMPA</h2>";
+echo "<table border='1'>";
+echo "<tr><th> NO. </th><th>TANGGAL</th><th>JAM</th><th>DATE TIME</th><th>KOORDINATE</th><th>LINTANG</th>
+<th>BUJUR</th><th>MAGNITUDE</th><th>KEDALAMAN</th><th>WILAYAH</th><th>DIRASAKAN</th></tr>";
+$counter = 1 ;
+foreach($gempa->gempa as $gmp){
+    
+    $Tanggal = $gmp->Tanggal;
+    $Jam = $gmp->Jam;
+    $DateTime = $gmp->DateTime;
+    $coordinates = $gmp->point->coordinates;
+    $Lintang = $gmp->Lintang;
+    $Bujur = $gmp->Bujur;
+    $Magnitude = $gmp->Magnitude;
+    $Kedalaman = $gmp->Kedalaman;
+    $Wilayah = $gmp->Wilayah;
+    $Dirasakan = $gmp->Dirasakan;
+
+    echo "<tr>";
+    echo "<td>$counter</td><td>$Tanggal</td><td>$Jam</td><td>$DateTime</td><td>$coordinates</td><td>$Lintang</td><td>$Bujur</td><td>$Magnitude</td><td>$Kedalaman</td><td>$Wilayah</td><td>$Dirasakan</td>";
+    echo "</tr>";
+  $counter++;
+}
+
+echo "</table>";
 ?>
