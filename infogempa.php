b066e522158a5ba72cf52e076fe45eb181002ce2
Dimas Agung Prabowo <96804772+dimasagp1@users.noreply.github.com>
2024-03-08 08:46:28 +0700

    Update infogempa.php

--- infogempa.php
+++ infogempa.php
@@ -1,27 +1,38 @@
+
 <?php
-/// Cara membaca/memparshing data json //
-$url="https://data.bmkg.go.id/DataMKG/TEWS/gempadirasakan.json";
-$data=file_get_contents($url);
-$result=json_decode($data);
-//echo "<pre/>";print_r($result);
-echo "<h2> Info Gempa TERKINI </h2>";
-$tmp="<table border='1'>";
-$tmp.="<tr><td><b>Tanggal</b></td><td><b>Keterangan</b></td></tr>";
-//contoh lain inisialisasi $datagempa->Infogempa->gempa;
-//echo  $datagempa[$i]->Tanggal."</br>";
-for($i=0;$i<count($result->Infogempa->gempa);$i++) {
-    //$no=$i+1
-    $tmp.="<tr><td>{$result->Infogempa->gempa[$i]->Tanggal}</td>
-        <td><b>Waktu : {$result->Infogempa->gempa[$i]->Jam}</b><br>
-         <i><b>Tanggal : </b>{$result->Infogempa->gempa[$i]->DateTime}</i><br>
-        <i><b>Koordinat : </b>{$result->Infogempa->gempa[$i]->Coordinates}</i><br>
-        <i><b>Lintang : </b>{$result->Infogempa->gempa[$i]->Lintang}</i><br>
-        <i><b>Bujur : </b>{$result->Infogempa->gempa[$i]->Bujur}</i><br>
-        <i><b>Magnitude : </b>{$result->Infogempa->gempa[$i]->Magnitude}</i><br>
-        <i><b>Kedalam : </b>{$result->Infogempa->gempa[$i]->Kedalaman}</i><br>
-        <i><b>Wilayah : </b>{$result->Infogempa->gempa[$i]->Wilayah}</i><br>
-     Dirasakan : {$result->Infogempa->gempa[$i]->Dirasakan}</td>";   
-}
-$tmp.="</table>";
-echo $tmp;
+$url="https://data.bmkg.go.id/DataMKG/TEWS/gempadirasakan.xml";
+$gempa= new SimpleXMLElement ($url,null,true);
+//echo "<pre>";print_r($gempa);
+
+echo "<h2>INFO GEMPA</h2>";
+$tmp="<table border='1'>
+        <tr><td> TANGGAL </td>
+        <td> JAM </td>
+        <td> TANGGAL WAKTU </td>
+        <td> KOORDINAT </td>
+        <td> LINTANG </td>
+        <td> BUJUR </td>
+        <td> Magnitude </td>
+        <td> KEDALAMAN </td>
+        <td> WILAYAH </td>
+        <td> DIRASAKAN </td>
+        <tr>";
+
+    for($i=0;$i<count($gempa->gempa);$i++) {
+        $tmp.="
+        <tr><td>{$gempa->gempa[$i]->Tanggal}</td>
+        <td><b>{$gempa->gempa[$i]->Jam}</b></td>
+        <td><b>{$gempa->gempa[$i]->DateTime}</b></td>
+        <td><b>{$gempa->gempa[$i]->point->coordinates}</b></td>
+        <td><b>{$gempa->gempa[$i]->Lintang}</b></td>
+        <td><b>{$gempa->gempa[$i]->Bujur}</b></td>
+        <td><b>{$gempa->gempa[$i]->Magnitude}</b></td>
+        <td><b>{$gempa->gempa[$i]->Kedalaman}</b></td>
+        <td><b>{$gempa->gempa[$i]->Wilayah}</b></td>
+        <td><b>{$gempa->gempa[$i]->Dirasakan}</b></td>
+        
+        </tr>";
+    }
+    $tmp.="</table>"; 
+    echo $tmp;
 ?>
